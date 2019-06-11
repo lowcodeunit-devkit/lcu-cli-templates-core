980769a18aa4f054e38d0402a3f83fac67a0e582
xchanin <me@shannonbruns.com>
2019-06-11 15:36:32 -0600

    change app

--- project/App/_sys/inquir.js
+++ project/App/_sys/inquir.js
@@ -5,7 +5,7 @@
       name: 'initWith',
       message: `Choose app initialization type:`,
       choices: [
-        'LCU Core',
+        'LCU Core App',
         'Default',
         'Blank',
       ]
