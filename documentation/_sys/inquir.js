3faf95c130f69154d04a8e83998151d563e598cc
Robert Wise <robert.wise@fathym.com>
2020-02-14 16:37:06 -0700

    Added new questions that ask user if they want to generate component files and routing

--- documentation/_sys/inquir.js
+++ documentation/_sys/inquir.js
@@ -8,6 +8,21 @@
         'Default',
         'LCU'
       ]
+    },
+    {
+      type: 'confirm',
+      name: 'includeComponent',
+      message: `Do you want to include the component files necessary for displaying the documentation?`,
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'includeRouting',
+      message: `Do you want to include a route for the new documentation component?`,
+      default: false,
+      when: (answers) => {
+        return answers.includeComponent;
+      }
     }
   ]
 }
